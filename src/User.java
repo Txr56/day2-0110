42d8123cbac0aa9c268f0d1f10312335f90e04a5
Administrator <1941187843@qq.com>
2021-01-10 17:46:14 +0800

    继承

---
+++ src/User.java
@@ -0,0 +1,18 @@
+public class User {
+    public User(int id, String userName) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+    String userName;
+    int id;
+    String password;
+    public User(int id,String userName,String password) {
+
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+    }
+}
---
+++ src/student.java
@@ -0,0 +1,12 @@
+public class student extends User{
+
+    public student (int id,String userName) {
+        super(id,userName);
+        id = 1;
+        System.out.println(id);
+        //System.out.println(id1);
+    }
+    public static void main(String[] args) {
+
+    }
+}
