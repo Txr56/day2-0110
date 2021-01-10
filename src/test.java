5c3a963c8559810984b55414e382b5f58349c1c5
Administrator <1941187843@qq.com>
2021-01-10 17:44:34 +0800

    继承

---
+++ src/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        String stu = new String("小明");
+        String  stuNumber = new String("2018108788");
+        //System.out.println(stu);
+
+
+    }
+}
